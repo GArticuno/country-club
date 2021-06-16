854ade5d6a15e79df99cd00d08a435e970a6478e
Mateus Romão <m.romao.ds@gmail.com>
2021-06-15 23:24:28 -0300

    add interface Props on _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { ServerStyleSheet } from 'styled-components';
 
-export default class Mydocument extends Document {
+interface Props {
+  styleTags: any
+}
+
+export default class Mydocument extends Document<Props> {
     static getInitialProps({ renderPage }) {
 
     const sheet = new ServerStyleSheet();
