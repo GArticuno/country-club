2feb1f250610de36f863eaa818e4432dac566f05
Mateus Romão <m.romao.ds@gmail.com>
2021-06-15 23:03:06 -0300

    Create .babelrc file; Add babel-plugin-styled-components; Add ServerStyleSheet on _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+import { ServerStyleSheet } from 'styled-components';
+
 export default class Mydocument extends Document {
+    static getInitialProps({ renderPage }) {
+
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
   render() {
     return (
       <Html lang="en-US">
@@ -9,6 +23,7 @@ export default class Mydocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet"/>
           <link rel="icon" href="/favicon.ico" />
+	  {this.props.styleTags}
         </Head>
         <body>
           <Main />
